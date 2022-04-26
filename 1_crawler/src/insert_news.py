2da0f1a1f4a0a3c04a9ca4e8d84ee7356816821b
Benedikt Schwering <benedikt.schwering@flender.com>
2022-04-26 16:09:00 +0200

    advanced crawler

---
+++ 1_crawler/src/insert_news.py
@@ -0,0 +1,81 @@
+from mongo import TagesschauMongoDB
+from datetime import datetime
+from time import sleep
+import requests
+
+class InsertNews():
+
+    def __init__(self) -> None:
+        self.db = TagesschauMongoDB()
+ 
+        while True:
+            self.news_output_bucket = []
+            
+            self.__crawl_articles()
+            self.db.insert_news(self.news_output_bucket)
+            
+            sleep(5 * 60)
+
+    def __crawl_articles(self) -> None:
+        frontpage_news = requests.get('https://www.tagesschau.de/api2').json()['news']
+
+        all_news_raw = requests.get('https://www.tagesschau.de/api2/news').json()
+        all_news = all_news_raw['news'] + requests.get(all_news_raw['nextPage']).json()['news']
+
+        self.__insert_frontpage_news(frontpage_news)
+        self.__insert_all_news(all_news)
+
+    def __insert_frontpage_news(self, frontepage_news: list) -> None:
+        for index, news in enumerate(frontepage_news):
+            
+            if news.get('sophoraId', None):
+                
+                existing_news = self.db.get_news(news['sophoraId'])
+
+                if not existing_news:
+                    news['crawler'] = {
+                        'insertTime': datetime.now(),
+                        'crawlTime': datetime.now(),
+                        'crawlType': 'insert',
+                        'frontpageIndex': index,
+                        'frontpageStatus': 'in',
+                        'allIndex': -1,
+                        'allStatus': 'stay out'
+                    }
+
+                    self.news_output_bucket.append(news)
+
+    def __insert_all_news(self, all_news: list) -> None:
+        for index, news in enumerate(all_news):
+            
+            if news.get('sophoraId', None):
+                
+                existing_news_db = self.db.get_news(news['sophoraId'])
+                
+                try:
+                    existing_news_bucket = [x for x in self.news_output_bucket if x['sophoraId'] == news['sophoraId']][0]
+                except:
+                    existing_news_bucket = None
+
+                if not existing_news_db and not existing_news_bucket:
+                    try:
+                        news = requests.get(news['details']).json()
+                    except:
+                        continue
+
+                    news['crawler'] = {
+                        'insertTime': datetime.now(),
+                        'crawlTime': datetime.now(),
+                        'crawlType': 'insert',
+                        'frontpageIndex': -1,
+                        'frontpageStatus': 'stay out',
+                        'allIndex': index,
+                        'allStatus': 'in'
+                    }
+
+                    print('Inserted article: ', news['sophoraId'])
+                    self.news_output_bucket.append(news)
+
+                elif existing_news_bucket:
+                    existing_news_bucket['crawler']['allIndex'] = index
+                    existing_news_bucket['crawler']['allStatus'] = 'in'
---
+++ 1_crawler/src/main.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv, find_dotenv
+from insert_news import InsertNews
+from update_news import UpdateNews
+from time import sleep
+import threading
+
+load_dotenv(find_dotenv())
+
+insert_news = threading.Thread(target=InsertNews)
+insert_news.daemon = False
+insert_news.start()
+
+sleep(120)
+
+update_news = threading.Thread(target=UpdateNews)
+update_news.daemon = False
+update_news.start()
