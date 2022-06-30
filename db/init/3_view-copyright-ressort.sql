308bd62042563f2fd5b19db40e218acb4cbdadca
Benedikt Schwering <benedikt.schwering@flender.com>
2022-06-30 08:55:24 +0200

    copyright_resort

--- db/init/3_view-copyright-ressort.sql
+++ db/init/3_view-copyright-ressort.sql
@@ -2,4 +2,9 @@ DROP VIEW IF EXISTS public.copyright_ressort;
 
 CREATE VIEW public.copyright_ressort AS 
 
-	
+select 
+    c.*,
+    t.ressort
+from clean_copyright as c
+left join tagesschau as t
+	on t.id = c."tagesschauId";
