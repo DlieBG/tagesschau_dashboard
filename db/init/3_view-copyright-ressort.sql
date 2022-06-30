a27cf019b8a937e586f9b9a4beea36a4cab087b5
Benedikt Schwering <benedikt.schwering@flender.com>
2022-06-30 09:54:57 +0200

    ressort hack

--- db/init/3_view-copyright-ressort.sql
+++ db/init/3_view-copyright-ressort.sql
@@ -7,4 +7,31 @@ select
     t.ressort
 from clean_copyright as c
 left join tagesschau as t
-	on t.id = c."tagesschauId";
+	on t.id = c."tagesschauId"
+where 
+	t.ressort is not null and
+	t.ressort != 'investigativ' and
+	t."crawlerCrawlType" = 'insert'
+	
+union 
+
+select 
+    c.*,
+	case 
+		when c."imageCopyright" = 'dpa' then 'inland'
+		when c."imageCopyright" = 'picture alliance' then 'ausland'
+		when c."imageCopyright" = 'IMAGO' then 'inland'
+		when c."imageCopyright" = 'Associated Press' then 'ausland'
+		when c."imageCopyright" = 'Reuters' then 'ausland'
+		when c."imageCopyright" = 'Agence France-Presse' then 'ausland'
+		when c."imageCopyright" = 'Freie Journalisten' then 'ausland'
+		when c."imageCopyright" = 'Getty Images' then 'sport'
+		when c."imageCopyright" = 'ARD' then 'inland'
+	else 'other'
+	end as ressort
+from clean_copyright as c
+left join tagesschau as t
+	on t.id = c."tagesschauId"
+where 
+	t.ressort is null and
+	t."crawlerCrawlType" = 'insert';
